167ba26289f222d35b70dc6ce3f9385c44048687
Ayushman Chhabra <ayushmxn@outlook.com>
2021-01-17 14:32:57 -0500

    Upload project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tabula Rasa
+
+Tabula Rasa transforms Roman Hindi to Devanagari Hindi using Google Translate API. I frquently write Hindi poetry. I am forced to write on my mobile which has the Hindi keyboard. I am much faster on a desktop. My solution is to write it in Roman Hindi, translate it into Devanagari Hindi and export it from there.
+
+## Technical Stack
+
+- React v17
+- Node v15
+- Google Translate API
+
+## Setup
+
+- Clone repository
+- ```cd``` into ```./app```
+- ```npm install``` dependencies
+- ```npm start``` to run app
+- go to [http://localhost:3000](http://localhost:3000)
---
+++ app/src/components/Page.js
@@ -0,0 +1,45 @@
+import React from "react";
+import '../assets/css/Page.css';
+import GCP from '../config/google-cloud-translate';
+
+export default class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      output: '',
+      target: 'hi'
+    };
+    this.onChange = this.onChange.bind(this);
+    this.translate = this.translate.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  translate() {
+    console.log(this.state.input);
+    let url = `https://translation.googleapis.com/language/translate/v2?key=${GCP.KEY}`;
+    url += '&q=' + encodeURI(this.state.input);
+    url += `&target=${this.state.target}`;
+
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => { this.setState({ output: data.data.translations[0].translatedText}); })
+      .catch(error => { console.log(error) });
+  }
+
+  render() {
+    return (<div>
+      <textarea value={this.state.input} onChange={this.onChange} placeholder="Write in Roman Hindi" name="input"></textarea>
+      <textarea value={this.state.output} placeholder="View in Devanagari Hindi"></textarea>
+      <button onClick={this.translate}>Transform</button>
+    </div>);
+  }
+}
